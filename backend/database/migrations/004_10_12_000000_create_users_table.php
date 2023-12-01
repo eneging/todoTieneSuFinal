dc510c8befe01809088ac4fda68922ea58b83312
eneging <140635337+eneging@users.noreply.github.com>
2023-12-01 12:14:45 -0500

    commit

---
+++ backend/database/migrations/004_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('idusuario');
+            $table->unsignedBigInteger('idpersona')->nullable();
+            $table->string('usuario');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('habilitado')->default(true);
+            $table->string('fecha')->nullable();
+            $table->unsignedBigInteger('idrol')->nullable();
+            $table->string('fechamodificacion')->nullable();
+            $table->string('usuariocreacion')->nullable();
+            $table->string('usuariomodificacion')->nullable();
+            $table->rememberToken();
+            $table->foreign('idpersona')->references('idpersona')->on('personas');
+            $table->foreign('idrol')->references('idrol')->on('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
